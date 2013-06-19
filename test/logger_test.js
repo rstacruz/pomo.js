148e13a25f45d95e93970ba2972aa55633b327a3
Rico Sta. Cruz <hi@ricostacruz.com>
2013-06-20 01:32:03 +0800

    Refactor to put reporters in lib/reporter/.

--- test/logger_test.js
+++ test/logger_test.js
@@ -32,10 +32,11 @@ describe('logger', function() {
    */
 
   it('should work fresh', function() {
-    logger('x.txt', {
+    logger.log('x.txt', {
       reason: 'working',
       duration: 35*mins, 'break': 5*mins,
-      date: moment('May 5, 2013 1:00 pm').toDate()
+      start: moment('May 5, 2013 1:00 pm').toDate(),
+      end: moment('May 5, 2013 1:40 pm').toDate()
     });
 
     var args = logger.write.firstCall.args;
@@ -50,10 +51,11 @@ describe('logger', function() {
   });
 
   it('no break', function() {
-    logger('x.txt', {
+    logger.log('x.txt', {
       reason: 'working',
       duration: 35*mins, 'break': 0,
-      date: moment('May 5, 2013 1:00 pm').toDate()
+      start: moment('May 5, 2013 1:00 pm').toDate(),
+      end: moment('May 5, 2013 1:35 pm').toDate()
     });
 
     var args = logger.write.firstCall.args;
@@ -64,22 +66,25 @@ describe('logger', function() {
     });
   });
 
-  it('should consolidate', function() {
-    logger('x.txt', {
+  it('should consolistart', function() {
+    logger.log('x.txt', {
       reason: 'working',
       duration: 35*mins, 'break': 5*mins,
-      date: moment('May 5, 2013 3:00 pm').toDate()
+      start: moment('May 5, 2013 3:00 pm').toDate(),
+      end: moment('May 5, 2013 3:40 pm').toDate()
     });
-    logger('x.txt', {
+    logger.log('x.txt', {
       reason: 'working again',
       duration: 30*mins, 'break': 5*mins,
-      date: moment('May 5, 2013 3:30 pm').toDate(),
+      start: moment('May 5, 2013 4:00 pm').toDate(),
+      end: moment('May 5, 2013 4:35 pm').toDate(),
       interrupted: true
     });
-    logger('x.txt', {
+    logger.log('x.txt', {
       reason: 'also working',
       duration: 25*mins, 'break': 5*mins,
-      date: moment('May 6, 2013 5:00 am').toDate()
+      start: moment('May 6, 2013 5:00 am').toDate(),
+      end: moment('May 6, 2013 5:30 am').toDate()
     });
 
     var args = logger.write.thirdCall.args;
@@ -87,7 +92,7 @@ describe('logger', function() {
     assert.jsonEqual(logger.load(args[1]), {
       '2013-05-05 sun': {
         '3:00pm - 3:40pm': 'working (35m + 5m)',
-        '3:30pm - 4:05pm': 'working again (30m + 5m, stopped)'
+        '4:00pm - 4:35pm': 'working again (30m + 5m, stopped)'
       },
       '2013-05-06 mon': {
         '5:00am - 5:30am': 'also working (25m + 5m)'
